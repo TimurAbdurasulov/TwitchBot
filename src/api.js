d1e4128e403e30b4de9999737d5b13bd13a4a25c
Timur <sver40ck@gmail.com>
2020-04-24 01:30:47 -0400

    second commit

--- src/api.js
+++ src/api.js
@@ -1,16 +1,35 @@
 const express = require('express')
-
 const serverless = require('serverless-http')
-
 const app = express()
-
 const router = express.Router();
+const tmi = require('tmi.js');
+
+const client = new tmi.client({
+    identity: {
+      username: 'ue_ban',
+      password: 'oauth:wd3tjzf2w0mb9ei2dvwla6wz8fsr42'
+    },
+    channels: [
+      'idiotexception'
+    ]
+});
+
+client.on('message', onMessageHandler);
+
+client.connect();
+
+function onMessageHandler (target, context, msg, self) {
+    if (self) { return; } // Ignore messages from the bot
+    client.say(target, `You rolled a ${num}`);
+    client.say('idiotexception', 'test test');
+}
 
 router.get('/', (req,res)=>{
     res.json({
         'hello': 'hi!'
     })
 })
+
 
 app.use('/.netlify/functions/api', router)
 
